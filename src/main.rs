b16c580dc5291dbf54d5f867bdd5ca0799001671
Chen Weiguang <chen.weiguang@gmail.com>
2019-10-08 08:14:44 +0800

    Comment out some functions

--- src/main.rs
+++ src/main.rs
@@ -68,24 +68,60 @@ const HELLO_DIR_ATTR: FileAttr = FileAttr {
     flags: 0,
 };
 
-const HELLO_TXT_CONTENT: &str = "Hello World!\n";
-
-const HELLO_TXT_ATTR: FileAttr = FileAttr {
+const HELLO_DIR_ATTR_OTHER: FileAttr = FileAttr {
     ino: 2,
-    size: 13,
+    size: 0,
     blocks: 1,
     atime: UNIX_EPOCH, // 1970-01-01 00:00:00
     mtime: UNIX_EPOCH,
     ctime: UNIX_EPOCH,
     crtime: UNIX_EPOCH,
-    kind: FileType::RegularFile,
-    perm: 0o644,
+    kind: FileType::Directory,
+    perm: 0o755,
     nlink: 1,
     uid: 501,
     gid: 20,
     rdev: 0,
     flags: 0,
 };
+
+fn create_tag_attr(ino: u64) -> FileAttr {
+    FileAttr {
+        ino,
+        size: 0,
+        blocks: 1,
+        atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+        mtime: UNIX_EPOCH,
+        ctime: UNIX_EPOCH,
+        crtime: UNIX_EPOCH,
+        kind: FileType::Directory,
+        perm: 0o755,
+        nlink: 1,
+        uid: 501,
+        gid: 20,
+        rdev: 0,
+        flags: 0,
+    }
+}
+
+const HELLO_TXT_CONTENT: &str = "Hello World!\n";
+
+// const HELLO_TXT_ATTR: FileAttr = FileAttr {
+//     ino: 2,
+//     size: 13,
+//     blocks: 1,
+//     atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+//     mtime: UNIX_EPOCH,
+//     ctime: UNIX_EPOCH,
+//     crtime: UNIX_EPOCH,
+//     kind: FileType::RegularFile,
+//     perm: 0o644,
+//     nlink: 1,
+//     uid: 501,
+//     gid: 20,
+//     rdev: 0,
+//     flags: 0,
+// };
 
 struct GhaFs {
     state: GitHub,
@@ -100,44 +136,47 @@ impl GhaFs {
 }
 
 impl Filesystem for GhaFs {
-    fn lookup(
-        &mut self,
-        _req: &Request,
-        parent: u64,
-        name: &OsStr,
-        reply: ReplyEntry,
-    ) {
-        if parent == 1
-            && name.to_str() != Some(".")
-            && name.to_str() != Some("..")
-        {
-            reply.entry(&TTL, &HELLO_DIR_ATTR, 0);
-        } else {
-            reply.error(ENOENT);
-        }
-    }
+    // fn lookup(
+    //     &mut self,
+    //     _req: &Request,
+    //     parent: u64,
+    //     name: &OsStr,
+    //     reply: ReplyEntry,
+    // ) {
+    //     println!("Parent: {}", parent);
+    //     println!("Name: {}", name.to_string_lossy());
+    //     // println!("Request: {:?}", req);
+
+    //     if parent == 1
+    //         && name.to_str() != Some(".")
+    //         && name.to_str() != Some("..")
+    //     {
+    //         reply.entry(&TTL, &HELLO_DIR_ATTR, 0);
+    //     } else {
+    //         reply.error(ENOENT);
+    //     }
+    // }
+
+    // fn read(
+    //     &mut self,
+    //     _req: &Request,
+    //     ino: u64,
+    //     _fh: u64,
+    //     offset: i64,
+    //     _size: u32,
+    //     reply: ReplyData,
+    // ) {
+    //     if ino == 2 {
+    //         reply.data(&HELLO_TXT_CONTENT.as_bytes()[offset as usize..]);
+    //     } else {
+    //         reply.error(ENOENT);
+    //     }
+    // }
 
     fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
         match ino {
             1 => reply.attr(&TTL, &HELLO_DIR_ATTR),
-            2 => reply.attr(&TTL, &HELLO_TXT_ATTR),
-            _ => reply.error(ENOENT),
-        }
-    }
-
-    fn read(
-        &mut self,
-        _req: &Request,
-        ino: u64,
-        _fh: u64,
-        offset: i64,
-        _size: u32,
-        reply: ReplyData,
-    ) {
-        if ino == 2 {
-            reply.data(&HELLO_TXT_CONTENT.as_bytes()[offset as usize..]);
-        } else {
-            reply.error(ENOENT);
+            x @ _ => reply.attr(&TTL, &create_tag_attr(x)),
         }
     }
 
@@ -149,6 +188,7 @@ impl Filesystem for GhaFs {
         offset: i64,
         mut reply: ReplyDirectory,
     ) {
+        // Root is ino == 1
         if ino != 1 {
             reply.error(ENOENT);
             return;
@@ -159,9 +199,9 @@ impl Filesystem for GhaFs {
             .releases(&self.owner, &self.repo)
             .expect("Releases GET error");
 
-        let tags = releases
-            .into_iter()
-            .map(|r| (1, FileType::Directory, r.tag_name));
+        let tags = releases.into_iter().enumerate().map(|(idx, r)| {
+            ((idx + 2) as u64, FileType::Directory, r.tag_name)
+        });
 
         let entries = vec![
             (1, FileType::Directory, ".".to_owned()),
